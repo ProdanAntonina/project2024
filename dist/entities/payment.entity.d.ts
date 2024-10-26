2c34e13f70bc288da779e5a891143756139bc865
Antonina <96832186+ProdanAntonina@users.noreply.github.com>
2024-10-26 15:46:25 +0300

    Delete dist directory

--- dist/entities/payment.entity.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class Payment {
-    id: number;
-    payment_method: string;
-    payment_status: string;
-    payment_date: Date;
-    total_price: number;
-}
--- dist/entities/products.entity.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=products.entity.js.map
